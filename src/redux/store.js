f5d5642a44c70f1d0965febeefef2315f916504e
Еремин Александр Владимирович <alex.eremin.official@gmail.com>
2022-11-20 12:58:13 +0300

    optimalz

--- src/redux/store.js
+++ src/redux/store.js
@@ -1,10 +1,8 @@
-import { configureStore } from '@reduxjs/toolkit';
-import { counterSlice } from './counter';
+import { configureStore } from '@reduxjs/toolkit'
 import { typeModal } from './typeModal';
 
 export const store = configureStore({
 	reducer: {
-		counter: counterSlice.reducer,
 		typeModal: typeModal.reducer,
 	},
 });
